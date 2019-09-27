787f1e37c125071318ad32ed22cdd09a7caf04fa
pkly <kakolp95@gmail.com>
2019-09-27 14:17:10 +0200

    Basic test implementation

---
+++ tests/BaseTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pkly
+ * Date: 27.09.2019
+ * Time: 14:15
+ */
+
+namespace Pkly\I18Next\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase {
+
+}
