5964701ec16c78179ce81ff25237ede8e0f4f53c
pkly <kakolp95@gmail.com>
2019-09-30 14:19:42 +0200

    Update InterpolatorTest

--- tests/InterpolatorTest.php
+++ tests/InterpolatorTest.php
@@ -13,9 +13,10 @@ use PHPUnit\Framework\TestCase;
 use Pkly\I18Next\Interpolator;
 
 class InterpolatorTest extends TestCase {
-    public function testInterpolation() {
+    public function testBasicInterpolation() {
         $interpolator = new Interpolator();
 
         $this->assertEquals('__test__/__data__', $interpolator->interpolate('__{{val1}}__/__{{val2}}__', ['val1' => 'test', 'val2' => 'data']));
+        $this->assertEquals('____/__test__/__data__', $interpolator->interpolate('__{{val1}}__/__{{val2}}__/__{{val3}}__', ['val2' => 'test', 'val3' => 'data']));
     }
 }
